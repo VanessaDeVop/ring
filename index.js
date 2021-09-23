c32838aaf528b3f43902724c5d4706c0b6c69ddb
VanessaDevop <webdev.vgp@gmail.com>
2021-09-23 17:44:21 +0200

    ring

---
+++ index.js
@@ -0,0 +1,7 @@
+const lower = ('the lord of the rings: the return of the king');
+
+const toTitleCase = require("to-title-case");
+
+const title = toTitleCase("The Lord of the Rings: The Return of the King");
+
+console.log(title)
